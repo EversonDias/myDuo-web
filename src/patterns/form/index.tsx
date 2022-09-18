15028787578890170db4f3c6cd987499ba5c4cf4
EversonDias <eversond14s@gmail.com>
2022-09-17 23:40:24 -0300

    add checkbox

--- src/patterns/form/index.tsx
+++ src/patterns/form/index.tsx
@@ -1,16 +1,38 @@
 import * as Dialog from "@radix-ui/react-dialog"
-import { GameController } from "phosphor-react"
+import { Check, GameController } from "phosphor-react"
 import LabelInput from "../../components/labelInput"
+import * as Checkbox from "@radix-ui/react-checkbox"
+import { useEffect, useState } from 'react';
+
+interface Game {
+    id: string;
+    title: string;
+}
 
 export default function Forms() {
+    const [games, setGames] = useState<Game[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:3333/games')
+            .then(response => response.json())
+            .then(data => {
+                setGames(data)
+            })
+    }, [])
+
     return (
         <form className="mt-8">
-            <LabelInput
+            <select
                 id='game'
-                key='game'
-                type="text"
-                text="Qual o game?"
-                placeholder="Selecione o game que deseja jogar" />
+                className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 "
+            >
+                <option disabled selected value='' >Selecione o game que deseja jogar</option>
+                {games.map(game => {
+                    return <option key={game.id} value={game.id}>{game.title}</option>
+                })}
+            </select>
+
+
             <LabelInput
                 key='name'
                 id="name"
@@ -107,23 +129,27 @@ export default function Forms() {
             </div>
 
 
-            <div className="mt-4 flex gap-2 text-sm ">
-                <input
-                    type="checkbox"
-                />
+            <div className="mt-4 flex iten gap-2 text-sm ">
+
+                <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+                    <Checkbox.Indicator>
+                        <Check className="w-4 h-4 text-emerald-400" />
+                    </Checkbox.Indicator>
+                </Checkbox.Root>
+
                 Costumo me conectar ao chat de voz
             </div>
 
             <footer className="mt-4 flex justify-between">
-                    <Dialog.Close  type="button" className="bg-zinc-500 px-5 h-12 rounded-md hover:bg-zinc-600">
-                        Cancelar
-                    </Dialog.Close>
-                    <button 
+                <Dialog.Close type="button" className="bg-zinc-500 px-5 h-12 rounded-md hover:bg-zinc-600">
+                    Cancelar
+                </Dialog.Close>
+                <button
                     className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
                     type="submit">
-                        <GameController size={24}/>
-                        Encontrar duo
-                    </button>
+                    <GameController size={24} />
+                    Encontrar duo
+                </button>
             </footer>
         </form>
     )
